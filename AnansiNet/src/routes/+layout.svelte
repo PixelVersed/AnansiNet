152dea36f5f6eb67a91b8abf08a6d7764d9076e2
Emir-Maesker <emir.maesker@gmail.com>
2025-11-07 23:53:01 +0100

    fix(theme): force dark mode as default and disable system preference

--- AnansiNet/src/routes/+layout.svelte
+++ AnansiNet/src/routes/+layout.svelte
@@ -1,16 +1,21 @@
 <script lang="ts">
   import "../app.css";
+  import { onMount } from "svelte";
 
   // Components
-  import { ModeWatcher } from "mode-watcher";
+  import { ModeWatcher, setMode } from "mode-watcher";
   import * as Sidebar from "$lib/components/ui/sidebar/index.js";
   import AppSidebar from "$lib/components/navigation/app-sidebar.svelte";
   import AppHeader from "$lib/components/navigation/app-header.svelte";
 
   let { children } = $props();
+
+  onMount(() => {
+    setMode("dark");
+  });
 </script>
 
-<ModeWatcher />
+<ModeWatcher track={false} />
 <Sidebar.Provider>
   <AppSidebar />
   <main class="w-full h-full">
